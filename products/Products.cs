d79fdb04a7d066378d3fe102c2d3965893b73b24
pdesgn <pdesgn@DESKTOP-LC4MO2N>
2021-02-19 00:32:04 +0300

    Add project files.

---
+++ products/Products.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace products
+{
+    class Products
+    {
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
---
+++ products/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Products product1 = new Products();
+            product1.ProductName = "Apple Macbook Air";
+            product1.ProductPrice = 1500;
+            product1.UnitsInStock = 1000;
+
+            Products product2 = new Products();
+            product2.ProductName = "Apple Macbook Pro";
+            product2.ProductPrice = 2500;
+            product2.UnitsInStock = 1000;
+
+            Products[] products = new Products[] { product1, product2 };
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.ProductName + " "  +   product.ProductPrice +  "$");
+            }
+        }
+
+
+
+
+
+       
+    }
+}
